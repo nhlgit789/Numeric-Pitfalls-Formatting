67d2fea73bc9d0387461d5a6acdbfa40bea3cf43
User <User@LAPTOP-9J7EIFSL>
2025-09-27 15:07:51 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+int a = 5, b = 2;
+Console.WriteLine(a / b); // 2 (int division truncates)
+Console.WriteLine(a / (double)b); // 2.5 (promote to double)
+double avg = (5 + 2 + 3) / 3.0;
+Console.WriteLine($"{avg:F2}"); // 3.33
